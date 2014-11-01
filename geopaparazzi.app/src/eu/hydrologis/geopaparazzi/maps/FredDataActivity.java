e0712386b2989e91e238e101726d10e2f2a97d62
Tim Howard <tghoward@gmail.com>
2014-11-01 07:03:05 -0400

    modify flag activity for fred

--- geopaparazzi.app/src/eu/hydrologis/geopaparazzi/maps/FredDataActivity.java
+++ geopaparazzi.app/src/eu/hydrologis/geopaparazzi/maps/FredDataActivity.java
@@ -316,10 +316,10 @@ public class FredDataActivity extends Activity {
                 public void onClick( View v ) {
                     Intent intent = new Intent("com.syware.droiddb"); //$NON-NLS-1$
                     intent.addFlags(intent.FLAG_ACTIVITY_NEW_TASK);
-                    intent.addFlags(intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                    //intent.addFlags(intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
                     //intent.addFlags(intent.FLAG_ACTIVITY_SINGLE_TOP);
                     // intent.addFlags(intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-                    intent.putExtra("parameter", externalDBname); //$NON-NLS-1$
+                    //intent.putExtra("parameter", externalDBname); //$NON-NLS-1$
                     // can I put form name as a parameter?
                     startActivity(intent);
                 }
