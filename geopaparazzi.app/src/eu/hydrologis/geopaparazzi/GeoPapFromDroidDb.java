6fb6ef821963d059e0dd440e19c14ec60c555bdc
tghoward <tghoward@gmail.com>
2015-01-10 05:44:05 -0500

    add onClose onDestroy for new activity

--- geopaparazzi.app/src/eu/hydrologis/geopaparazzi/GeoPapFromDroidDb.java
+++ geopaparazzi.app/src/eu/hydrologis/geopaparazzi/GeoPapFromDroidDb.java
@@ -87,9 +87,19 @@ public class GeoPapFromDroidDb extends Activity{
         super.onPause();
     }
 
+    @Override
     protected void onResume() {
 
         super.onResume();
     }
-    
+
+    @Override
+    protected void onStop() {
+
+        super.onStop();
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+    }
 }
